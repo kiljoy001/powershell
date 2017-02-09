f485afdd0cc938ab3048e17249b5192a918a6b92
Scott Guyton <kiljoy001@gmail.com>
2017-02-09 15:53:21 -0800

    powershell automation
    added rough draft of the powershell automation script

---
+++ powershelltrial.ps1
@@ -0,0 +1,23 @@
+﻿while ($true)
+{
+    # check for process1
+    if((Get-Process -Name python) -eq $null)
+    {
+        #check if chromiumn is running
+        if((Get-Process -Name chromium) -eq !$null)
+        {
+            sleep -s 10  
+        }
+
+    }
+    else
+    {
+        # check if chromium is still running, if so close.
+        if((Get-Process -Name chromium) -eq !$null)
+        {
+            (Stop-Process -Name chromium | Wait-Process)
+            cd "C:\Users\User\PycharmProjects\keysurveyscrape\Key Survey Webscrape"
+            python processing.py
+        }
+    }
+}
